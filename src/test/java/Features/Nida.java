79fd615f59a6766d853e38adb1f4b9f95b108288
nida2004 <nyilmaz2025@gmail.com>
2020-10-08 17:45:47 +0200

    branch nida

---
+++ src/test/java/Features/Nida.java
@@ -0,0 +1,16 @@
+package Features;
+
+public class Nida {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("This is new request");
+
+
+        System.out.println("new print for branch");
+    }
+
+
+
+}
